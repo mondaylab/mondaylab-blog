a7d371fe4815bf849e08cd63e0c6cc1ddb58316b
Jacqueline712 <60708406+Jacqueline712@users.noreply.github.com>
2022-10-15 19:03:38 +0800

    update: 重命名目录和更新配置项

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,123 +1,137 @@
 const { sidebar } = require("./sidebar.js");
 module.exports = {
-  title: '星期一研究室mondaylab',
-  description: '分享前沿学习干货，不止前端',
-  base: '/mondaylab-blog/',
-  theme: 'reco',
+  title: "星期一研究室mondaylab",
+  description: "分享前沿学习干货，不止前端",
+  base: "/mondaylab-blog/",
+  theme: "reco",
   // 移动端搜索框优化
   head: [
-    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
   ],
   markdown: {
     toc: {
       // includeLevel 默认只显示 [2, 3]
-      includeLevel: [1, 2, 3, 4]
+      includeLevel: [1, 2, 3, 4],
     },
-    extractHeaders: [ 'h1', 'h2', 'h3', 'h4' ]
+    extractHeaders: ["h1", "h2", "h3", "h4"],
   },
   themeConfig: {
     // 取消404公益
     noFoundPageByTencent: false,
     nav: [
       {
-        text: '首页',
-        link: '/',
-        icon: "reco-home"
+        text: "首页",
+        link: "/",
+        icon: "reco-home",
       },
       {
-        text: '精选专栏',
+        text: "精选专栏",
         items: [
           {
-            text: '数构与前端',
+            text: "数构与前端",
             // 此处读到README.md文件中，或者下方sidebar中对应的链接
-            link: '/column/Algorithm/'
+            link: "/column/Algorithm/",
           },
           {
-            text: 'offer来了',
-            link: '/column/Interview/'
-          }
-        ],
-        icon: "reco-tag"
-      },
-      {
-        text: '干货社区',
-        items: [
-          {
-            text: '基本操作',
-            link: '/column/BaseCommand/'
-          },
-          {
-            text: '模板管理',
-            link: '/column/Template/'
+            text: "offer来了",
+            link: "/column/Interview/",
           },
         ],
-        icon: "reco-eye"
+        icon: "reco-tag",
       },
       {
-        text: '分类索引',
+        text: "干货社区",
         items: [
-          
           {
-            text: '产品汪',
-            link: '/column/Product/'
+            text: "基本操作",
+            link: "/column/BaseCommand/",
           },
           {
-            text: '前端开发',
-            link: '/column/FrontEnd/'
+            text: "模板管理",
+            link: "/column/Template/",
+          },
+        ],
+        icon: "reco-eye",
+      },
+      {
+        text: "分类索引",
+        items: [
+          {
+            text: "产品汪",
+            link: "/column/Product/",
           },
           {
-            text: '其他语言',
-            link: '/column/OtherLang/'
+            text: "前端开发",
+            link: "/column/FrontEnd/",
           },
           {
-            text: '服务端开发',
-            link: '/column/BackEnd/'
+            text: "其他语言",
+            link: "/column/OtherLang/",
           },
           {
-            text: '计算机基础知识',
+            text: "服务端开发",
+            link: "/column/BackEnd/",
+          },
+          {
+            text: "计算机基础知识",
             // 此处读到README.md文件中，或者下方sidebar中对应的链接
-            link: '/column/ComputerKnowledge/'
+            link: "/column/ComputerKnowledge/",
           },
           {
-            text: '碎碎念个人成长',
-            link: '/column/Growing/'
-          }
+            text: "碎碎念个人成长",
+            link: "/column/Growing/",
+          },
         ],
-        icon: "reco-category"
+        icon: "reco-category",
       },
       {
-        text: '关于我',
+        text: "关于我",
         items: [
-          { text: 'Github', link: 'https://github.com/Jacqueline712' },
-          { text: '掘金', link: 'https://juejin.cn/user/3131845139247960/posts' },
-          { text: 'CSDN', link: 'https://blog.csdn.net/weixin_44803753' },
-          { text: 'InfoQ', link: 'https://www.infoq.cn/profile/3AC1A95FEC468A/publish' },
-          { text: '知乎', link: 'https://www.zhihu.com/people/zheng-zi-ji-67-89/posts' },
+          { text: "Github", link: "https://github.com/Jacqueline712" },
+          {
+            text: "掘金",
+            link: "https://juejin.cn/user/3131845139247960/posts",
+          },
+          { text: "CSDN", link: "https://blog.csdn.net/weixin_44803753" },
+          {
+            text: "InfoQ",
+            link: "https://www.infoq.cn/profile/3AC1A95FEC468A/publish",
+          },
+          {
+            text: "知乎",
+            link: "https://www.zhihu.com/people/zheng-zi-ji-67-89/posts",
+          },
           // { text: '公众号', link: 'https://blog.csdn.net/weixin_44803753' }
         ],
-        icon: "reco-account"
-      }
+        icon: "reco-account",
+      },
     ],
     sidebar,
     // 让所有页面都有目录
     // sidebar: 'auto',
     // 更换时间格式
     locales: {
-      '/': {
-        lang: 'zh-CN'
-      }
+      "/": {
+        lang: "zh-CN",
+      },
     },
     type: "blog",
     // 标签设置
     // blogConfig: {
-      // category: {
-      //   location: 2,
-      //   text: "分类索引",
-      // },
-      // tag: {
-      //   location: 3,
-      //   text: "标签索引",
-      // },
+    // category: {
+    //   location: 2,
+    //   text: "分类索引",
+    // },
+    // tag: {
+    //   location: 3,
+    //   text: "标签索引",
+    // },
     // },
     // 此处放导航栏最左侧的logo
     logo: "/avatar.png",
@@ -127,8 +141,8 @@ module.exports = {
     author: "SyllableCheng",
     authorAvatar: "/avatar.png",
     // 备案
-    record: 'vuepress-theme-reco@1.6.10',
+    record: "vuepress-theme-reco@1.6.10",
     // 项目开始时间，只填写年份
-    startYear: '2022'
-  }
-}
+    startYear: "2022",
+  },
+};
---
+++ docs/column/BaseCommand/MacShortcutKey/004_google.md
@@ -0,0 +1,14 @@
+---
+title: Google控制台快捷键
+author: 周一
+date: '2022-09-24'
+categories:
+  - 学习干货
+tags:
+  - Google快捷键
+sidebar: 'auto'
+---
+
+# 通用快捷键
+
+https://support.google.com/chrome/answer/157179?hl=zh-Hans&co=GENIE.Platform%3DDesktop&oco=1#zippy=%2C%E6%A0%87%E7%AD%BE%E9%A1%B5%E5%92%8C%E7%AA%97%E5%8F%A3%E5%BF%AB%E6%8D%B7%E9%94%AE%2Cgoogle-chrome-%E5%8A%9F%E8%83%BD%E5%BF%AB%E6%8D%B7%E9%94%AE%2C%E5%9C%B0%E5%9D%80%E6%A0%8F%E5%BF%AB%E6%8D%B7%E9%94%AE%2C%E7%BD%91%E9%A1%B5%E5%BF%AB%E6%8D%B7%E9%94%AE%2C%E9%BC%A0%E6%A0%87%E5%BF%AB%E6%8D%B7%E9%94%AE
