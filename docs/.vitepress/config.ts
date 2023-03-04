46ccc378b78ef5bf4b9dd093e05ae0669f7a75a9
Jacqueline712 <60708406+Jacqueline712@users.noreply.github.com>
2023-03-04 22:33:52 +0800

    add:将vuepress框架改为vitepress框架

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,56 @@
+/**
+ * @description 基础配置
+ */
+import { defineConfig } from "vitepress";
+import { sidebar, nav } from "./relaConf";
+import { demoBlockPlugin } from "vitepress-theme-demoblock";
+const anchor = require("markdown-it-anchor"); // markdown的库
+
+export default defineConfig({
+  // 基本配置
+  base: process.env.BASE || "/mondaylab-blog/", // 项目的基础路径
+  title: "星期一研究室mondaylab", // 文档的标题
+  description: "分享前沿学习干货，不止前端", // 文档描述
+  lastUpdated: true,
+
+  // 移动端搜索框优化
+  head: [
+    [
+      "meta",
+      {
+        name: "viewport",
+        content: "width=device-width,initial-scale=1,user-scalable=no",
+      },
+    ],
+    ["link", { rel: "icon", type: "image/svg+xml", href: "/logo.svg" }],
+  ],
+
+  // 配置markdown写作风格
+  markdown: {
+    toc: {
+      // level 默认只显示 [2, 3]
+      level: [1, 2, 3, 4],
+    },
+    // light: #f9fafb, dark: --vp-code-block-bg
+    theme: { light: "github-light", dark: "github-dark" },
+
+    config: (md) => {
+      md.use(demoBlockPlugin, {
+        cssPreprocessor: "less",
+      });
+    },
+  },
+
+  // 主题配置
+  themeConfig: {
+    nav,
+    sidebar,
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+    footer: {
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2022-present 君惜",
+    },
+  },
+});
---
+++ docs/.vitepress/relaConf/navbar.ts
@@ -0,0 +1,98 @@
+import { DefaultTheme } from "vitepress";
+
+export const nav: DefaultTheme.NavItem[] = [
+  {
+    text: "首页",
+    link: "/",
+  },
+  {
+    text: "个人成长",
+    items: [
+      {
+        text: "大江南北游记",
+        link: "/column/Travel/",
+      },
+      {
+        text: "所思·所想",
+        link: "/column/Growing/",
+      },
+    ],
+  },
+  {
+    text: "精选专栏",
+    items: [
+      {
+        text: "数构与前端",
+        // 此处读到README.md文件中，或者下方sidebar中对应的链接
+        link: "/column/Algorithm/",
+      },
+      {
+        text: "offer来了",
+        link: "/column/Interview/",
+      },
+    ],
+  },
+  {
+    text: "干货社区",
+    items: [
+      {
+        text: "快捷键",
+        link: "/column/BaseCommand/",
+      },
+      {
+        text: "模板管理",
+        link: "/column/Template/",
+      },
+    ],
+  },
+  {
+    text: "分类索引",
+    items: [
+      {
+        text: "产品汪",
+        link: "/column/Product/",
+      },
+      {
+        text: "前端开发",
+        link: "/column/FrontEnd/",
+      },
+      {
+        text: "其他语言",
+        link: "/column/OtherLang/",
+      },
+      {
+        text: "服务端开发",
+        link: "/column/BackEnd/",
+      },
+      {
+        text: "计算机基础知识",
+        // 此处读到README.md文件中，或者下方sidebar中对应的链接
+        link: "/column/ComputerKnowledge/",
+      },
+      {
+        text: "碎碎念个人成长",
+        link: "/column/Growing/",
+      },
+    ],
+  },
+  {
+    text: "关于我",
+    items: [
+      { text: "Github", link: "https://github.com/Jacqueline712" },
+      {
+        text: "掘金",
+        link: "https://juejin.cn/user/3131845139247960/posts",
+      },
+      {
+        text: "飞书社区",
+        link: "https://pzfqk98jn1.feishu.cn/drive/folder/fldcnPSJjrOFADFHehZtlzv8AnN",
+      },
+      { text: "CSDN", link: "https://blog.csdn.net/weixin_44803753" },
+      {
+        text: "知乎",
+        link: "https://www.zhihu.com/people/zheng-zi-ji-67-89/posts",
+      },
+      // { text: '公众号', link: 'https://blog.csdn.net/weixin_44803753' }
+    ],
+  },
+];
